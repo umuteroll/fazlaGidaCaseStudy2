2804205354afb7d2d57ef2b95807408732fd757a
Umut Erol <umut_erol1999@hotmail.com>
2021-10-20 22:45:54 +0300

    header and flatList addeed

--- App.js
+++ App.js
@@ -1,13 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {FlatList, SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <SafeAreaView style={{backgroundColor:"#d73963",
+                            width:"100%",
+                            height:"50px"
+    }}>
+      
+      <Text
+       style={{fontSize:20,
+        margin:"15px",
+        color: "#ffff",
+        fontFamily:"sans-serif"
+               
+       
+       }}>
+         Random Users</Text>
+      </SafeAreaView>
+      
+      <FlatList
+        data={[
+          {key: 'Devin'},
+          {key: 'Dan'},
+          {key: 'Dominic'},
+          {key: 'Jackson'},
+          {key: 'James'},
+          {key: 'Joel'},
+          {key: 'John'},
+          {key: 'Jillian'},
+          {key: 'Jimmy'},
+          {key: 'Julie'},
+        ]}
+        renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+      />
     </View>
+    
   );
 }
 
