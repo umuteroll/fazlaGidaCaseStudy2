62b1a914170d47b53e3f7cf6f948b130899cc211
Umut Erol <umut_erol1999@hotmail.com>
2021-10-22 01:09:22 +0300

    screens are seperated

--- App.js
+++ App.js
@@ -2,96 +2,28 @@ import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useState } from 'react';
 import Icon from 'react-native-vector-icons/Ionicons';
 import { Avatar } from 'react-native-elements';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {HomeScreen} from './screens/Home';
+import {DetailsScreen} from './screens/Detail';
 import { RefreshControl, FlatList, SafeAreaView, StyleSheet, Text, View, ViewBase } from 'react-native';
 
+const Stack = createNativeStackNavigator();
+
 export default function App() {
-  
-    const [contacts, setContacts] = useState([])
-    const [refreshing, setRefreshing] = React.useState(false);
-    useEffect( () => {
-      fetch('https://randomuser.me/api/?results=10')
-      .then(resp => resp.json())
-      .then(resp => setContacts(resp.results))
 
-
-    }, [])
-    
-    const onRefresh = React.useCallback(async () => {
-      setRefreshing(true);
-      useEffect( () => {
-        fetch('https://randomuser.me/api/?results=10')
-        .then(resp => resp.json())
-        .then(resp => setContacts(resp.results))
-  
-  
-      }, [])
-    }, [refreshing]);
     
    
 
   return (
-    <View style={styles.container}>
-      <SafeAreaView style={{
-        backgroundColor: "#d73963",
-        width: "100%",
-        height: "50px"
-      }}>
-
-        <Text
-          style={{
-            fontSize: 20,
-            margin: "15px",
-            color: "#ffff",
-            fontFamily: "sans-serif"
-
-
-          }}>
-          Random Users</Text>
-      </SafeAreaView>
-
-      <FlatList
-        data={contacts}
-        style={{margin:"10px"}}
-        refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}
-        renderItem={({ item }) => 
-        <View style={{ flexDirection:"row"}}>
-        <Avatar
-            rounded
-            containerStyle={{ marginTop:"12px", marginRight:"5px"}}
-            source={{
-              uri:
-                item.picture.thumbnail,
-            }}
-          />
-        <View style={{ minHeight: 70, padding: 5 }}>
-          <Text style={{ color: 'black', fontWeight: 'bold', fontSize: 26 }}>
-            {item.name.first + ' '}
-            {item.name.last }
-          </Text>
-          <Text style={{ color: 'grey', fontWeight: 'bold' }}>
-            {item.email}
-          </Text>
-        </View>
-        <View style={{  flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'flex-end',
-        alignItems: 'center',
-        marginBottom:"15px"
-        
-        }}>
-        <Icon name={item.gender+"-outline"} size={25} color="black" />
-          </View>
-        
-        </View>
-        }
-      />
-    </View>
+    <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Details" component={DetailsScreen} />
+    </Stack.Navigator>
+  </NavigationContainer>
 
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    
-  },
-});
+
